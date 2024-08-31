6e80bf2ca308e671dd80be4b47c5421b5f9ab30f
ItalyanecXXX <anatoly.bezrukoff@gmail.com>
2024-08-31 18:02:39 +0700

    cub

---
+++ main.js
@@ -0,0 +1,13 @@
+new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true,
+
+    navigation: {
+        nextEl: '#ligue__button-next',
+        prevEl: '#ligue__button-back',
+    },
+
+
+});
